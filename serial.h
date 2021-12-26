5fb6ce7e374fb79799b4f4359c93fe74c894344a
Thomas Roth <code@stacksmashing.net>
2021-12-26 17:21:39 +0100

    Add serial console

---
+++ serial.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// FIFO commands
+#define cmd_arm 0
+#define cmd_disarm 1
+#define cmd_pulse 2
+#define cmd_status 3
+#define cmd_enable_timeout 4
+#define cmd_disable_timeout 5
+#define return_ok 0
+#define return_failed 1
+
+void serial_console();
